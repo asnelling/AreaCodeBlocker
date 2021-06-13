b122f63d273ba84271fcdf41f847f3c00ddfcfc1
Addison Snelling <addison@asnell.io>
2021-06-13 05:47:24 -0500

    code cleanup

--- app/src/main/java/io/asnell/areacodeblocker/AreaCodesApplication.kt
+++ app/src/main/java/io/asnell/areacodeblocker/AreaCodesApplication.kt
@@ -5,8 +5,12 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.SupervisorJob
 
 class AreaCodesApplication : Application() {
-    val applicationScope = CoroutineScope(SupervisorJob())
-
-    val database by lazy { AreaCodeRoomDatabase.getDatabase(this, applicationScope) }
+    private val applicationScope = CoroutineScope(SupervisorJob())
+    private val database by lazy {
+        AreaCodeRoomDatabase.getDatabase(
+            this,
+            applicationScope
+        )
+    }
     val repository by lazy { AreaCodeRepository(database.areaCodeDao()) }
 }
