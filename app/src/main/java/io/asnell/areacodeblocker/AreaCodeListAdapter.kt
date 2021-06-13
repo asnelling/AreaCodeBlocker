22a056305e8afc010533aa0431cb413b12e8623a
Addison Snelling <addison@asnell.io>
2021-06-13 11:42:58 -0500

    add an icon to indicate the configured action for each area code

--- app/src/main/java/io/asnell/areacodeblocker/AreaCodeListAdapter.kt
+++ app/src/main/java/io/asnell/areacodeblocker/AreaCodeListAdapter.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
@@ -35,12 +36,29 @@ class AreaCodeListAdapter : ListAdapter<AreaCode, AreaCodeListAdapter.AreaCodeVi
     }
 
     class AreaCodeViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val resources = itemView.resources
+        private val actionImage: ImageView = itemView.findViewById(R.id.action_image)
         private val areaCodeItemView: TextView = itemView.findViewById(R.id.areaCode)
         val removeAreaCodeButton: ImageButton = itemView.findViewById(R.id.remove)
 
         fun bind(areaCode: AreaCode) {
             areaCodeItemView.text = areaCode.code
             removeAreaCodeButton.tag = areaCode
+
+            when(areaCode.action) {
+                Action.DISALLOW -> {
+                    actionImage.setImageResource(R.drawable.ic_baseline_block_24)
+                    actionImage.contentDescription = itemView.resources.getString(R.string.call_block)
+                }
+                Action.REJECT -> {
+                    actionImage.setImageResource(R.drawable.ic_baseline_phone_missed_24)
+                    actionImage.contentDescription = itemView.resources.getString(R.string.call_reject)
+                }
+                Action.SILENCE -> {
+                    actionImage.setImageResource(R.drawable.ic_baseline_notifications_off_24)
+                    actionImage.contentDescription = itemView.resources.getString(R.string.call_silence)
+                }
+            }
         }
 
         companion object {
