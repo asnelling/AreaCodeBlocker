1115dfd4907565589f99d953c15f5a8ae5b588c5
Addison Snelling <addison@asnell.io>
2021-07-07 21:11:56 -0500

    change database test to a local rather than instrumented unit test
    now the tests can run on your dev machine instead of waiting to spin up the
    emulator.

--- app/src/test/java/io/asnell/prefixscreener/PrefixDaoTest.kt
+++ app/src/test/java/io/asnell/prefixscreener/PrefixDaoTest.kt
@@ -17,7 +17,7 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-class PrefixDbTest {
+class PrefixDaoTest {
     private lateinit var db: AppDatabase
     private lateinit var prefixDao: PrefixDao
 
